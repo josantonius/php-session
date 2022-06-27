c57ed44865b6ffd922262fb1801e6a874281bdfb
Josantonius <hello@josantonius.dev>
2022-06-27 22:02:03 +0200

    feat: handle exceptions
    #12

---
+++ src/Exceptions/SessionException.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of https://github.com/josantonius/php-session repository.
+ *
+ * (c) Josantonius <hello@josantonius.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Josantonius\Session\Exceptions;
+
+/**
+ * You can use an exception and error handler with this library.
+ *
+ * @link https://github.com/josantonius/php-errorhandler
+ */
+class SessionException extends \Exception
+{
+    public function __construct(string $message = 'Unknown error')
+    {
+        parent::__construct(rtrim($message, '.') . '.');
+    }
+}
