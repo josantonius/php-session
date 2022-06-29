720489b866f9574e0a92f5a73bffbbce16b46402
Josantonius <hello@josantonius.dev>
2022-06-29 23:51:05 +0200

    refactor: Replace symbol
    Replace symbol in start method comment.

--- src/Facades/Session.php
+++ src/Facades/Session.php
@@ -31,36 +31,36 @@ class Session
     /**
      * Starts the session.
      *
-     * List of available $options with their default values.
+     * List of available $options with their default values:
+     *
+     * * cache_expire: "180"
+     * * cache_limiter: "nocache"
+     * * cookie_domain: ""
+     * * cookie_httponly: "0"
+     * * cookie_lifetime: "0"
+     * * cookie_path: "/"
+     * * cookie_samesite: ""
+     * * cookie_secure: "0"
+     * * gc_divisor: "100"
+     * * gc_maxlifetime: "1440"
+     * * gc_probability: "1"
+     * * lazy_write: "1"
+     * * name: "PHPSESSID"
+     * * read_and_close: "0"
+     * * referer_check: ""
+     * * save_handler: "files"
+     * * save_path: ""
+     * * serialize_handler: "php"
+     * * sid_bits_per_character: "4"
+     * * sid_length: "32"
+     * * trans_sid_hosts: $_SERVER['HTTP_HOST']
+     * * trans_sid_tags: "a=href,area=href,frame=src,form="
+     * * use_cookies: "1"
+     * * use_only_cookies: "1"
+     * * use_strict_mode: "0"
+     * * use_trans_sid: "0"
      *
      * @see https://php.net/session.configuration
-     *
-     * cache_expire: "180",
-     * cache_limiter: "nocache",
-     * cookie_domain: "",
-     * cookie_httponly: "0",
-     * cookie_lifetime: "0",
-     * cookie_path: "/",
-     * cookie_samesite: "",
-     * cookie_secure: "0",
-     * gc_divisor: "100",
-     * gc_maxlifetime: "1440",
-     * gc_probability: "1",
-     * lazy_write: "1",
-     * name: "PHPSESSID",
-     * read_and_close: "0",
-     * referer_check: "",
-     * save_handler: "files",
-     * save_path: "",
-     * serialize_handler: "php",
-     * sid_bits_per_character: "4",
-     * sid_length: "32",
-     * trans_sid_hosts: $_SERVER['HTTP_HOST'],
-     * trans_sid_tags: "a=href,area=href,frame=src,form=",
-     * use_cookies: "1",
-     * use_only_cookies: "1",
-     * use_strict_mode: "0",
-     * use_trans_sid: "0".
      *
      * @throws SessionException if headers already sent
      * @throws SessionException if session already started
